dbde67d9fa0b194701aff229635885f82c939e09
murillokminamoto-afk <murillokminamoto@hotmail.com>
2026-03-24 15:15:55 -0300

    finaliza desafio calculadora de partidas ranqueadas

---
+++ Desafio2.js
@@ -0,0 +1,24 @@
+let vitorias = 78
+let derrotas = 23 
+function calcularSaldo(vitorias, derrotas) {
+    let saldo = vitorias - derrotas
+    return saldo
+}
+let saldo = calcularSaldo(vitorias, derrotas)
+let nivel
+if (vitorias < 10) {
+    nivel = "Ferro"
+}else if (vitorias < 20) {
+    nivel = "Bronze"
+}else if (vitorias < 50) {
+    nivel = "Prata"
+}else if (vitorias < 80) {
+    nivel = "Ouro"
+}else if (vitorias < 90) {
+    nivel = "Diamante"
+}else if (vitorias < 100) {
+    nivel = "Lendário"
+}else {
+    nivel = "Imortal"
+}
+console.log("O Herói tem de saldo de " + saldo + " está no nível de " + nivel)
